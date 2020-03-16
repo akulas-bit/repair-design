4c4ac16e5e769545ec9f625e963610448d369c5f
akulas-bit <anket77@yandex.ru>
2020-03-16 20:38:24 +0300

    feat lesson 19 and 20

--- js/main.js
+++ js/main.js
@@ -35,6 +35,18 @@ $(document).ready(function () {
       el: '.swiper-pagination',
       type: 'bullets',
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
   
 });
